1796a4ed570bfb016aa07354019e7764a49d7682
azibf <arinazakh@gmail.com>
2024-10-15 00:32:33 +0300

    clesring code

--- api.py
+++ api.py
@@ -1,28 +1,44 @@
 from proxmoxer import ProxmoxAPI
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.INFO)
+
 
 def nodes(ip, username, password):
-    proxmox = ProxmoxAPI(ip, user=username, password=password, verify_ssl=False, service='PVE')
+    proxmox = ProxmoxAPI(
+        ip, user=username, password=password, verify_ssl=False, service="PVE"
+    )
     return proxmox.nodes.get()
 
 
 def connect(vmids, ip, username, password, node):
-    proxmox = ProxmoxAPI(ip, user=username, password=password, verify_ssl=False, service='PVE')
-    node = proxmox.nodes([node])
-    pool = list(map(int, vmids.split(',')))
-    vms = []
-    for elem in node.qemu.get():
-        if elem['vmid'] in pool:
-            vms.append((node.qemu(elem['vmid']), elem))
-    return vms
+    try:
+        proxmox = ProxmoxAPI(
+            ip, user=username, password=password, verify_ssl=False, service="PVE"
+        )
+        node = proxmox.nodes([node])
+        pool = list(map(int, vmids.split(",")))
+        vms = []
+        for elem in node.qemu.get():
+            if elem["vmid"] in pool:
+                vms.append((node.qemu(elem["vmid"]), elem))
+        return vms
+    except:
+        logger.error("CONNECTION ERROR")
+
 
 def create(ip, username, password, node, golden_img, count):
-    proxmox = ProxmoxAPI(ip, user=username, password=password, verify_ssl=False, service='PVE')
+    proxmox = ProxmoxAPI(
+        ip, user=username, password=password, verify_ssl=False, service="PVE"
+    )
     node = proxmox.nodes([node])
-    max_vmid = 1 + max([vm['vmid'] for vm in node.qemu.get()])
+    max_vmid = 1 + max([vm["vmid"] for vm in node.qemu.get()])
     gi = node.qemu(golden_img)
     for i in range(max_vmid, max_vmid + count):
         gi.clone.post(newid=i)
     return [str(i) for i in range(max_vmid, max_vmid + count)]
+
 
 def checkPool(vmids, ip, username, password, node):
     pool = connect(vmids, ip, username, password, node)
@@ -30,52 +46,59 @@ def checkPool(vmids, ip, username, password, node):
     mem = 0
     for vm in pool:
         vm = vm[0].config().get()
-        cpu += vm['cores'] * vm['sockets']
-        mem += int(vm['memory'])
+        cpu += vm["cores"] * vm["sockets"]
+        mem += int(vm["memory"])
     return cpu, mem
+
 
 def getPool(vmids, ip, username, password, node):
     pool = connect(vmids, ip, username, password, node)
     info = []
     for vm in pool:
         info.append((vm[0].config().get(), vm[1]))
-    return(info)
-            
+    return info
+
+
 def delete(vmids, ip, username, password, node):
-    pool = connect(vmids, ip, username, password, node)
-    for vm in pool:
-        vm[0].delete()
-    
+    try:
+        pool = connect(vmids, ip, username, password, node)
+        for vm in pool:
+            vm[0].delete()
+    except:
+        logger.error("ERROR DURING DELETE")
+
+
 def stop(vmids, ip, username, password, node):
     pool = connect(vmids, ip, username, password, node)
     try:
         for vm in pool:
             vm[0].status.stop.post()
     except:
-        pass
-    
+        logger.error("ERROR DURING STOPING POOL")
+
+
 def start(vmids, ip, username, password, node):
     pool = connect(vmids, ip, username, password, node)
     try:
         for vm in pool:
             vm[0].status.start.post()
     except:
-        print("ERROR DURING STARTING POOL")
-    
+        logger.error("ERROR DURING STARTING POOL")
+
+
 def shutdown(vmids, ip, username, password, node):
     pool = connect(vmids, ip, username, password, node)
     try:
         for vm in pool:
             vm[0].status.shutdown.post()
     except:
-        print("ERROR DURING SHUTING DOWN")
-    
+        logger.error("ERROR DURING SHUTING DOWN")
+
+
 def reboot(vmids, ip, username, password, node):
     pool = connect(vmids, ip, username, password, node)
     try:
         for vm in pool:
             vm[0].status.reboot.post()
     except:
-        print("ERROR DURING REBOOTING DOWN")
-    
-    
+        logger.error("ERROR DURING REBOOTING DOWN")
--- data/models.py
+++ data/models.py
@@ -7,18 +7,17 @@ from flask_login import UserMixin
 
 
 class User(SqlAlchemyBase, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     username = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     ip = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    pools = orm.relationship("Pool", back_populates='user')
+    pools = orm.relationship("Pool", back_populates="user")
 
 
 class Pool(SqlAlchemyBase, UserMixin):
-    __tablename__ = 'pools'
+    __tablename__ = "pools"
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     count = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
@@ -26,6 +25,5 @@ class Pool(SqlAlchemyBase, UserMixin):
     naming = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     node = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     vmids = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    user_id = sqlalchemy.Column(sqlalchemy.Integer,
-                                sqlalchemy.ForeignKey("users.id"))
-    user = orm.relationship('User')
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship("User")
